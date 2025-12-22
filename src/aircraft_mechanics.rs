001ad496680ff4d3cc1e7935653237b10901018b
wesfly <wesfly@noreply.codeberg.org>
2025-12-22 14:51:04 +0100

    Simplify logic

--- src/aircraft_mechanics.rs
+++ src/aircraft_mechanics.rs
@@ -2,21 +2,20 @@ use crate::{Aircraft, InputAxis, RotationOfSubject};
 use bevy::prelude::*;
 
 pub fn aircraft_mechanics(
-    mut query: Query<&mut Transform, With<Aircraft>>,
+    mut transform: Single<&mut Transform, With<Aircraft>>,
     time: Res<Time>,
     input: Res<InputAxis>,
     mut rotation: ResMut<RotationOfSubject>,
 ) {
     let delta = time.delta_secs();
-    for mut transform in &mut query {
-        let rotation_x = Quat::from_rotation_x(input.x * delta);
-        let rotation_z = Quat::from_rotation_z(input.z * delta);
-        transform.rotate_local(rotation_x);
-        transform.rotate_local(rotation_z);
 
-        let forward = transform.back();
-        transform.translation += forward * delta * input.w * 10.;
+    let rotation_x = Quat::from_rotation_x(input.x * delta);
+    let rotation_z = Quat::from_rotation_z(input.z * delta);
+    transform.rotate_local(rotation_x);
+    transform.rotate_local(rotation_z);
 
-        rotation.0 = transform.rotation;
-    }
+    let forward = transform.back();
+    transform.translation += forward * delta * input.w * 10.;
+
+    rotation.0 = transform.rotation;
 }
