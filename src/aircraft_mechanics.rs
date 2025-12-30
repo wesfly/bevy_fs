77edabed9f34dd7b6702fb4e0a94f8c912e8775d
wesfly <wesfly@noreply.codeberg.org>
2025-12-30 19:50:30 +0100

    Migrate to Avian for better colliders

--- src/aircraft_mechanics.rs
+++ src/aircraft_mechanics.rs
@@ -1,15 +1,17 @@
 use crate::{Aircraft, InputAxis};
+use avian3d::prelude::*;
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::ExternalForce;
 
 pub fn aircraft_mechanics(
     transform: Single<&GlobalTransform, With<Aircraft>>,
-    mut force: Single<&mut ExternalForce, With<Aircraft>>,
+    mut query: Query<Forces, With<Aircraft>>,
     input: Res<InputAxis>,
 ) {
-    force.force = transform.up() * 1_200. * (input.throttle);
+    let force = transform.up() * 300. * (input.throttle);
+    let torque = Vec3::new(input.pitch, input.yaw * 2.5, input.roll);
 
-    let local_vec = Vec3::new(input.pitch, input.yaw, input.roll);
-    let world_vec = transform.rotation() * local_vec;
-    force.torque = world_vec * 500.;
+    for mut forces in &mut query {
+        forces.apply_force(force);
+        forces.apply_local_torque(torque * 40.);
+    }
 }
