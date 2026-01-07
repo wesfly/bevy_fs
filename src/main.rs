73788740c310b361414a39450d0c978ed040b515
wesfly <wesfly@noreply.codeberg.org>
2026-01-07 20:49:27 +0100

    Camera logic into seperate file

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ ignoring them.
 */
 
 mod aircraft_mechanics;
+mod camera;
 mod handle_custom_properties;
 mod input;
 
@@ -16,7 +17,6 @@ use bevy::dev_tools::fps_overlay::FpsOverlayPlugin;
 use bevy::{
     camera::Exposure,
     core_pipeline::tonemapping::Tonemapping,
-    input::mouse::{AccumulatedMouseMotion, MouseScrollUnit, MouseWheel},
     light::{CascadeShadowConfigBuilder, light_consts::lux},
     pbr::Atmosphere,
     post_process::{bloom::Bloom, motion_blur::MotionBlur},
@@ -24,8 +24,9 @@ use bevy::{
     render::view::Hdr,
     scene::SceneInstanceReady,
 };
+use camera::camera_controller;
 use handle_custom_properties::on_scene_spawn;
-use input::{GamepadSettings, Keymap};
+use input::GamepadSettings;
 use serde::{Deserialize, Serialize};
 use std::{f32::consts::FRAC_PI_2, fs, ops::Range};
 
@@ -228,70 +229,4 @@ fn play_animation_when_ready(
             }
         }
     }
-}
-
-fn camera_controller(
-    mut camera: Single<&mut Transform, With<Camera>>,
-    camera_settings: Res<CameraSettings>,
-    mouse_buttons: Res<ButtonInput<MouseButton>>,
-    mouse_motion: Res<AccumulatedMouseMotion>,
-    keyboard_input: Res<'_, ButtonInput<KeyCode>>,
-    keymap: Res<Keymap>,
-    mut projection: Single<&mut Projection, With<FollowCamera>>,
-    mut scroll_events: MessageReader<MouseWheel>,
-) {
-    let cockpit_cam: bool = camera_settings.view == 1;
-
-    let delta = mouse_motion.delta;
-    let delta_pitch;
-    let delta_yaw;
-
-    if cockpit_cam {
-        delta_pitch = -delta.y * camera_settings.pitch_speed;
-        delta_yaw = -delta.x * camera_settings.yaw_speed;
-    } else {
-        delta_pitch = delta.y * camera_settings.pitch_speed;
-        delta_yaw = delta.x * camera_settings.yaw_speed;
-    }
-
-    // Obtain the existing pitch, yaw, and roll values from the transform.
-    let (yaw, pitch, roll) = camera.rotation.to_euler(EulerRot::YXZ);
-
-    let pitch = (pitch + delta_pitch).clamp(
-        camera_settings.pitch_range.start,
-        camera_settings.pitch_range.end,
-    );
-
-    let yaw = yaw + delta_yaw;
-
-    if cockpit_cam {
-        if mouse_buttons.pressed(MouseButton::Right) {
-            camera.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
-        }
-        camera.translation = camera_settings.cockpit_default_position;
-    } else {
-        let target = camera_settings.follow_default_lookat;
-        if mouse_buttons.pressed(MouseButton::Right) {
-            camera.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
-        }
-        camera.translation = target - camera.forward() * camera_settings.orbit_distance;
-
-        if keyboard_input.just_pressed(keymap.reset_camera) {
-            camera.translation = camera_settings.follow_default_position;
-            camera.look_at(target, Vec3::Y);
-        }
-    }
-
-    let Projection::Perspective(perspective) = projection.as_mut() else {
-        return;
-    };
-
-    for event in scroll_events.read() {
-        match event.unit {
-            MouseScrollUnit::Line => perspective.fov -= event.y * 0.05,
-            MouseScrollUnit::Pixel => {}
-        }
-    }
-
-    perspective.fov = perspective.fov.clamp(0.1, std::f32::consts::FRAC_PI_2);
 }
