b411fdaa2941c7b232adf18e4833187b150e82c9
wesfly <wesfly@noreply.codeberg.org>
2026-01-09 14:37:12 +0100

    Adjust steering sensitivity

--- src/aircraft_mechanics.rs
+++ src/aircraft_mechanics.rs
@@ -7,11 +7,11 @@ pub fn aircraft_mechanics(
     mut query: Query<Forces, With<Aircraft>>,
     input: Res<InputAxis>,
 ) {
-    let force = transform.up() * 60000. * (input.throttle);
+    let force = transform.up() * 50000. * (input.throttle);
     let torque = Vec3::new(input.pitch, input.yaw * 2.5, input.roll);
 
     for mut forces in &mut query {
         forces.apply_force(force);
-        forces.apply_local_torque(torque * 100.);
+        forces.apply_local_torque(torque * 1200.);
     }
 }
