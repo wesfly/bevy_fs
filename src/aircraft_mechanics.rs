0002cfe15f64a83b53b0901115546335f9cf574c
wesfly <wesfly@noreply.codeberg.org>
2026-01-16 22:16:11 +0100

    Different torque factors for joystick/keyboard input.

--- src/aircraft_mechanics.rs
+++ src/aircraft_mechanics.rs
@@ -1,4 +1,4 @@
-use crate::{Aircraft, InputAxis};
+use crate::{Aircraft, InputAxis, Settings};
 use avian3d::prelude::*;
 use bevy::prelude::*;
 
@@ -6,12 +6,21 @@ pub fn aircraft_mechanics(
     transform: Single<&GlobalTransform, With<Aircraft>>,
     mut query: Query<Forces, With<Aircraft>>,
     input: Res<InputAxis>,
+    settings: Res<Settings>,
 ) {
+    // When controlling with buttons, the inertia is too high. This adjusts for that.
+    let torque_factor;
+    if settings.gamepad_enabled {
+        torque_factor = 1200.
+    } else {
+        torque_factor = 3000.
+    }
+
     let force = transform.up() * 45000. * (input.throttle);
     let torque = Vec3::new(input.pitch, input.yaw * 2.5, input.roll);
 
     for mut forces in &mut query {
         forces.apply_force(force);
-        forces.apply_local_torque(torque * 1200.);
+        forces.apply_local_torque(torque * torque_factor);
     }
 }
