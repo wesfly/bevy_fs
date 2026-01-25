283019c9ffa60d8b2749aab94f9269fefe6d9196
wesfly <wesfly@noreply.codeberg.org>
2026-01-25 15:06:00 +0100

    Refine flight model

--- src/aircraft_mechanics.rs
+++ src/aircraft_mechanics.rs
@@ -1,22 +1,14 @@
-use crate::{Aircraft, AircraftState, InputAxis, Settings};
+use crate::{Aircraft, AircraftState, InputAxis};
 use avian3d::prelude::*;
 use bevy::prelude::*;
 
+// TODO put this into aircraft.rs
 pub fn aircraft_mechanics(
     transform: Single<&GlobalTransform, With<Aircraft>>,
     mut query: Query<Forces, With<Aircraft>>,
     input: Res<InputAxis>,
-    settings: Res<Settings>,
     state: Res<AircraftState>,
 ) {
-    // When controlling with buttons, the inertia is too high. This adjusts for that.
-    let torque_factor;
-    if settings.gamepad.enabled {
-        torque_factor = 1200.
-    } else {
-        torque_factor = 3000.
-    }
-
     let thrust_factor;
 
     if state.engine_on {
@@ -26,10 +18,10 @@ pub fn aircraft_mechanics(
     }
 
     let force = transform.up() * thrust_factor * (input.throttle);
-    let torque = Vec3::new(input.pitch, input.yaw * 2.5, input.roll);
+    let torque = Vec3::new(input.pitch, input.yaw, input.roll);
 
     for mut forces in &mut query {
         forces.apply_force(force);
-        forces.apply_local_torque(torque * torque_factor);
+        forces.apply_local_torque(torque * 500.0);
     }
 }
