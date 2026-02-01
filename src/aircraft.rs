9e6c587c5f3bbf17417cd744a4d38d3def959ebf
wesfly <wesfly@noreply.codeberg.org>
2026-02-01 10:20:47 +0100

    Disable controls when engine is off

--- src/aircraft.rs
+++ src/aircraft.rs
@@ -134,19 +134,14 @@ pub fn mechanics(
     input: Res<InputAxis>,
     state: Res<AircraftState>,
 ) {
-    let thrust_factor;
-
     if state.engine_on {
-        thrust_factor = 64_000.
-    } else {
-        thrust_factor = 0.
-    }
+        let thrust_factor = 64_000.;
+        let force = transform.up() * thrust_factor * (input.throttle);
+        let torque = Vec3::new(input.pitch, input.yaw, input.roll);
 
-    let force = transform.up() * thrust_factor * (input.throttle);
-    let torque = Vec3::new(input.pitch, input.yaw, input.roll);
-
-    for mut forces in &mut query {
-        forces.apply_force(force);
-        forces.apply_local_torque(torque * 500.0);
+        for mut forces in &mut query {
+            forces.apply_force(force);
+            forces.apply_local_torque(torque * 500.0);
+        }
     }
 }
