b7d7140126ad692c2979ab018f9377c60002d2f1
wesfly <wesfly@noreply.codeberg.org>
2026-02-27 19:38:02 +0100

    Remove warning

--- src/aircraft/mechanics.rs
+++ src/aircraft/mechanics.rs
@@ -28,46 +28,48 @@ pub fn mechanics(
             }
         }
         AircraftTypes::Aeroplane => {
-            let input_torque = Vec3::new(input.pitch, input.yaw, input.roll);
-            force.apply_local_torque(input_torque * 500.5);
+            if state.engine_on {
+                let input_torque = Vec3::new(input.pitch, input.yaw, input.roll);
+                force.apply_local_torque(input_torque * 500.5);
 
-            let forward = transform.forward();
+                let forward = transform.forward();
 
-            let rho = rho();
+                let rho = rho();
 
-            let velocity = force.linear_velocity();
-            let velocity_dir = velocity.normalize_or_zero();
-            let speed: f32 = velocity.length();
+                let velocity = force.linear_velocity();
+                let velocity_dir = velocity.normalize_or_zero();
+                let speed: f32 = velocity.length();
 
-            force.apply_force(thrust(&input, &forward));
+                force.apply_force(thrust(&input, &forward));
 
-            // Angle of attack
-            let sin = forward.cross(velocity_dir).dot(transform.right().as_vec3());
-            let cos = forward.dot(velocity_dir);
-            let aoa = -sin.atan2(cos).to_degrees();
+                // Angle of attack
+                let sin = forward.cross(velocity_dir).dot(transform.right().as_vec3());
+                let cos = forward.dot(velocity_dir);
+                let aoa = -sin.atan2(cos).to_degrees();
 
-            let lift_coeff = match aoa {
-                d if d < 15.0 => d / 15.0 * 1.0 + 0.5,
-                d if d < 20.0 => 1.2 * (1.0 - (d - 15.0) / 5.0),
-                _ => 0.2, // stalled
-            };
+                let lift_coeff = match aoa {
+                    d if d < 15.0 => d / 15.0 * 1.0 + 0.5,
+                    d if d < 20.0 => 1.2 * (1.0 - (d - 15.0) / 5.0),
+                    _ => 0.2, // stalled
+                };
 
-            let parasitic_drag = velocity.powf(2.0) * 0.8 * forward.cross(velocity_dir);
-            let drag = (-velocity_dir * induced_drag(lift_coeff, rho, speed))
-                + (-velocity_dir * parasitic_drag);
-            force.apply_force(drag);
+                let parasitic_drag = velocity.powf(2.0) * 0.8 * forward.cross(velocity_dir);
+                let drag = (-velocity_dir * induced_drag(lift_coeff, rho, speed))
+                    + (-velocity_dir * parasitic_drag);
+                force.apply_force(drag);
 
-            // Stabilisation (idk)
-            let stability_thingy = stabilise(&transform, velocity_dir, speed);
-            // force.apply_local_angular_acceleration(stability_thingy);
+                // Stabilisation (idk)
+                let stability_thingy = stabilise();
+                force.apply_local_angular_acceleration(stability_thingy);
 
-            // L = Cl * p * (v^2/2) * A
-            // Lift = coefficient * density * (airspeed^2 / 2) * wing area
-            let wing_area = 49.0;
-            let airspeed = forward.dot(velocity_dir).clamp(0.0, 1.0) * speed;
-            let lift = lift(lift_coeff, airspeed, wing_area, transform.up(), rho);
-            force.apply_force(lift);
-        } // }
+                // L = Cl * p * (v^2/2) * A
+                // Lift = coefficient * density * (airspeed^2 / 2) * wing area
+                let wing_area = 49.0;
+                let airspeed = forward.dot(velocity_dir).clamp(0.0, 1.0) * speed;
+                let lift = lift(lift_coeff, airspeed, wing_area, transform.up(), rho);
+                force.apply_force(lift);
+            }
+        }
     }
 }
 
@@ -93,14 +95,8 @@ fn induced_drag(lift_coeff: f32, rho: f32, speed: f32) -> f32 {
     induced_drag
 }
 
-fn stabilise(transform: &GlobalTransform, velocity_dir: Vec3, speed: f32) -> Vec3 {
-    let stability_error = transform.forward().cross(velocity_dir);
-    let local_error = transform.rotation().inverse().mul_vec3(stability_error);
-    let snap_intensity = 0.0015;
-    let mut stability_torque = local_error * speed.powi(2) * snap_intensity;
-    stability_torque.x = 0.0;
-
-    stability_torque
+fn stabilise() -> Vec3 {
+    Vec3::ZERO
 }
 
 fn lift(lift_coeff: f32, airspeed: f32, wing_area: f32, up: Dir3, rho: f32) -> Vec3 {
